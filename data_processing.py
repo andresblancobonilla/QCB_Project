cae2ceb25126313c2f0ef634e7aeaabee85827d2
Andres Blanco Bonilla <afbb@princeton.edu>
2022-11-22 12:47:27 -0500

    initial code

---
+++ data_processing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# data_processing.py
+# Author: Andres Blanco Bonilla
+# Reads sequence data into Python for further analysis.
+#-----------------------------------------------------------------------
+
