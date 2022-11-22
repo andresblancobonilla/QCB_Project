cffd0fafcf0a10e2460528137f241924ebe3a69e
Andres Blanco Bonilla <afbb@princeton.edu>
2022-11-22 14:17:53 -0500

    updated data processing

--- data_processing.py
+++ data_processing.py
@@ -6,9 +6,13 @@
 # Reads sequence data into Python for further analysis.
 #-----------------------------------------------------------------------
 import sys
-import os
+import sqlite3
+from contextlib import closing
 import pandas
 from Bio import SeqIO
+
+
+DATABASE_URL = "file:sequence.sqlite?mode=ro"
 
 #-----------------------------------------------------------------------
 
@@ -22,8 +26,26 @@ def read_sequences(fasta_file_list):
         for seq_record in SeqIO.parse(fasta_file, "fasta"):
             # print(seq_record.seq)
             sequence_list.append(seq_record.seq)
+    
     sequence_df = pandas.DataFrame({"sequence":sequence_list})
     return sequence_df
+
+def get_location_id_date():
+    try:
+        with sqlite3.connect(DATABASE_URL, isolation_level=None,
+                             uri=True) as connection:
+            with closing(connection.cursor()) as cursor:
+                query = "SELECT key FROM offset_data;"
+                cursor.execute(query)
+                id_list = []
+                row = cursor.fetchone()
+                while row is not None:
+                    id_list.append(row[0])
+                return id_list
+
+    except Exception as ex:
+        print(sys.argv[0] + ": " + str(ex), file=sys.stderr)
+        sys.exit(1)
 
 
 
@@ -35,6 +57,7 @@ def main():
     fasta_file_list = [f"FASTA/{file_name}" for file_name in fasta_file_list]
     sequence_df = read_sequences(fasta_file_list)
     print(sequence_df)
+    print(len(sequence_df.index))
     # print(list(sequence_df["sequence"])[3])
 
 #-----------------------------------------------------------------------
