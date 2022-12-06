b5e068343b64c699801fdb8a507015be1d584ef3
George Tziampazis <george.tzi@princeton.edu>
2022-12-05 19:29:29 -0500

    test

--- data_align.py
+++ data_align.py
@@ -10,39 +10,40 @@ import sqlite3
 from contextlib import closing
 import pandas
 from Bio import SeqIO
+from Bio import Align
 import data_processing
 
 
 DATABASE_URL = "file:sequence.sqlite?mode=ro"
+refseq_file = ["refseq NC_045512.fasta"]
+refseq_file = [f"FASTA/{file_name}" for file_name in refseq_file]
+refseq_df = data_processing.read_sequences(refseq_file)
+refseq_str = refseq_df.sequence[0]
 
-# new change
+aligner = Align.PairwiseAligner()
+aligner.match_score = 1.0
+aligner.open_gap_score = -10
+aligner.extend_gap_score = -.5
 
 #-----------------------------------------------------------------------
 
-# Returns a df with one column of all the sequences 
-# in the list of input fasta files
-def align_sequences(fasta_file_list):
-    # SeqIO.index_db("sequence.idx", fasta_file_list, "fasta")
-    sequence_list = []
-    
-    for fasta_file in fasta_file_list:
-        for seq_record in SeqIO.parse(fasta_file, "fasta"):
-            # print(seq_record.seq)
-            sequence_list.append(seq_record.seq)
-    
-    sequence_df = pandas.DataFrame({"sequence":sequence_list})
-    return sequence_df
+# Returns an alignment object of the reference sequence with the query # sequence
+def align_to_refseq(query_seq):
+    alignments = aligner.align(refseq_str, query_seq)
+    for alignment in alignments:
+        return alignment
 
 
 def main():
+    # getting main 
+    fasta_file_list = ["part_1_gisaid.fasta", "part_2_gisaid.fasta",\
+        "part_3_gisaid.fasta", "part_4_gisaid.fasta",\
+            "part_5_gisaid.fasta", "part_6_gisaid.fasta"]
+    fasta_file_list = [f"FASTA/{file_name}" for file_name in fasta_file_list]
+    sequence_df = data_processing.read_sequences(fasta_file_list)
 
-    refseq_file = ["refseq NC_045512.fasta"]
-    refseq_file = [f"FASTA/{file_name}" for file_name in refseq_file]
-    sequence_df = data_processing.read_sequences(refseq_file)
-    refseq_str = sequence_df.sequence[0]
-    print(refseq_str)
-    print(sequence_df)
-    print(len(sequence_df.index))
+    aligned = align_to_refseq(sequence_df.sequence[5000])
+    print(aligned.substitutions)
 
 #-----------------------------------------------------------------------
 if __name__ == '__main__':
